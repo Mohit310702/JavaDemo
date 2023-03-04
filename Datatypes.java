503f04ea8bc4f4a8d9545cbf74979091c89e470b
Mohit310702 <mohitdhiman154@gamil.com>
2023-03-04 18:19:32 +0530

    commit

---
+++ Datatypes.java
@@ -0,0 +1,8 @@
+class Datatypes
+{
+    static public void main(String []args)
+    {
+        byte num = 127;
+        System.out.println(num);
+    }
+}
---
+++ Demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello Java");
+		System.out.println(args[1]);
+		System.out.println("args[0]");
+
+	}
+}
